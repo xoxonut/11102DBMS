9e519740994d7728d5ecddc6ed723b3beecfdc5e
Frank <frank.ankch17@gmail.com>
2023-05-15 22:17:32 +0800

    add font

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -8,13 +8,16 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
     # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
-
+    from . import db
+    db.init_app(app)
+    
     # a simple page that says hello
     @app.route('/')
     def index():    
--- flaskr/api/member.py
+++ flaskr/api/member.py
@@ -1,7 +1,15 @@
-from flask import Blueprint
-
+from flask import Blueprint,render_template
+from flaskr.db import get_db
+from random import randint
 bp = Blueprint('member', __name__, url_prefix='/member')
 
 @bp.route('/')
 def index():
-    return 'Member index page'
+    db = get_db()
+    # db.execute(
+    #                 "INSERT INTO user (username, password) VALUES (?, ?)",
+    #                 (str(randint(1,234)), '123456'),
+    # )
+    # db.commit()
+    user = db.execute("SELECT * FROM user").fetchone()
+    return render_template('hello.jinja',user=user[1])
