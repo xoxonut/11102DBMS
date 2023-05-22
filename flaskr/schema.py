c358c9f98744d5ece1479ea79271894a1b80a653
tanlikfeng <likfengtan@gmail.com>
2023-05-22 14:20:17 +0800

    Insert data into the table

--- flaskr/schema.py
+++ flaskr/schema.py
@@ -4,16 +4,16 @@ conn = sqlite3.connect("instance/flaskr.sqlite")
 cursor = conn.cursor()
 
 staff = """CREATE TABLE STAFF(
-            staff_id INT PRIMARY KEY NOT NULL,
-            manager_id INT,
+            staff_id INTEGER PRIMARY KEY NOT NULL,
+            manager_id INTEGER,
             name CHAR(20) NOT NULL, 
-            entry_date DATE,
+            entry_date TEXT,
             FOREIGN KEY(manager_id) REFERENCES STAFF(staff_id)
             )"""
 cursor.execute(staff)
 
 supplier = """CREATE TABLE SUPPLIER(
-                supplier_id INT PRIMARY KEY,
+                supplier_id INTEGER PRIMARY KEY,
                 name CHAR(20) NOT NULL,
                 email CHAR(30),
                 phone_number CHAR(10),
@@ -22,7 +22,7 @@ supplier = """CREATE TABLE SUPPLIER(
 cursor.execute(supplier)
 
 member = """CREATE TABLE MEMBER(
-                member_id INT PRIMARY KEY,
+                member_id INTEGER PRIMARY KEY,
                 name CHAR(20) NOT NULL,
                 email CHAR(30),
                 phone_number CHAR(10),
@@ -31,48 +31,52 @@ member = """CREATE TABLE MEMBER(
 cursor.execute(member)
 
 item = """CREATE TABLE ITEM(
-            item_id INT PRIMARY KEY,
+            item_id INTEGER PRIMARY KEY,
             name CHAR(20) NOT NULL,
             type CHAR(30),
-            stock INT,
-            unit_price INT
+            stock INTEGER,
+            unit_price INTEGER
             )"""
 cursor.execute(item)
 
 purchase_order = """CREATE TABLE PURCHASE_ORDER (
-                        p_order_id INT PRIMARY KEY,
-                        supplier_id INT,
-                        staff_id INT,
-                        p_order_date DATE,
+                        p_order_id INTEGER PRIMARY KEY,
+                        supplier_id INTEGER,
+                        staff_id INTEGER,
+                        p_order_date TEXT,
                         FOREIGN KEY (supplier_id) REFERENCES SUPPLIER(supplier_id),
                         FOREIGN KEY (staff_id) REFERENCES STAFF(staff_id)
                         )"""
 cursor.execute(purchase_order)
 
 sales_order = """CREATE TABLE SALES_ORDER (
-                    s_order_id INT PRIMARY KEY,
-                    staff_id INT,
-                    member_id INT,
-                    s_order_date DATE,
+                    s_order_id INTEGER PRIMARY KEY,
+                    staff_id INTEGER,
+                    member_id INTEGER,
+                    s_order_date TEXT,
                     FOREIGN KEY (staff_id) REFERENCES STAFF(staff_id),
                     FOREIGN KEY (member_id) REFERENCES MEMBER(member_id)
                     )"""
 cursor.execute(sales_order)
 
 increase = """CREATE TABLE INCREASE (
-                item_id INT PRIMARY KEY,
-                p_order_id INT,
-                unit_cost INT,
-                item_quantity INT,
+                item_id INTEGER,
+                p_order_id INTEGER,
+                unit_cost INTEGER,
+                item_quantity INTEGER,
+                PRIMARY KEY (item_id, p_order_id),
+                FOREIGN KEY (item_id) REFERENCES ITEM(item_id)
                 FOREIGN KEY (p_order_id) REFERENCES PURCHASE_ORDER(p_order_id)
                 )"""
 cursor.execute(increase)
 
 decrease = """CREATE TABLE DECREASE (
-                item_id INT PRIMARY KEY,
-                s_order_id INT,
-                item_quantity INT,
-                FOREIGN KEY (s_order_id) REFERENCES SALES_ORDER(s_order_id)
+                item_id INTEGER,
+                s_order_id INTEGER,
+                item_quantity INTEGER,
+                PRIMARY KEY (item_id, s_order_id),
+                FOREIGN KEY (item_id) REFERENCES ITEM(item_id)
+                 FOREIGN KEY (s_order_id) REFERENCES SALES_ORDER(s_order_id)
                 )"""
 cursor.execute(decrease)
 
