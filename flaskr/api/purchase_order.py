afff89daa64c3be8c8589b4fcdff357d4e6c8b70
owen-chw <c044066bo@gmail.com>
2023-05-27 15:22:20 +0800

    Complete create purchase_order 	modified:   flaskr/api/purchase_order.py TODO: after DB modified to rowid, need to modify: line 4, 55, 56, 57, 62, 75, 76, 83, 87, 88, 89, 90, 93, 100, 101, 102, 103

--- flaskr/api/purchase_order.py
+++ flaskr/api/purchase_order.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 from flaskr.db import get_db
+import datetime
 import random # this line should delete after DB modified to use rowid
 
 bp = Blueprint('purchase_order', __name__, url_prefix='/purchase_order')
@@ -49,21 +50,30 @@ def create_purchase_order():
     if (rows[0]['result'] != 1):
       return {"message": "This supplier doesn't exist!"}
 
-    # TODO:insert into purchase order
+    # insert into purchase order
+    p_order_date = datetime.datetime.now().strftime("%c")
+    p_order_id = random.randint(1000, 100000) # this line should delete after DB modified to rowid
+    cursor = db.execute("""INSERT INTO PURCHASE_ORDER(p_order_id, supplier_id, staff_id, p_order_date) 
+      VALUES (?,?,?,?)""", [p_order_id, supplier_id, staff_id, p_order_date]) # this line should modify after DB modified to rowid
+
+    cursor = db.execute("SELECT rowid FROM PURCHASE_ORDER WHERE supplier_id = ? AND staff_id = ? AND p_order_date=?", [supplier_id, staff_id, p_order_date])
+    rows = cursor.fetchall()
+    p_order_id = rows[0][0]
+    print("create order: "+str(p_order_id))
 
     for item in item_list:
       item_name = item.get('name')
       item_type = item.get('type')
       item_quantity = item.get('quantity')
       item_unit_cost = item.get('unit_cost')
+
       # check if item's name and type have existted. Otherwise, create item
       cursor = db.execute("SELECT COUNT(*) AS result FROM ITEM WHERE name = ? AND type = ?", [item_name, item_type])
       rows = cursor.fetchall()
       if (rows[0]['result'] != 1):
         #item doesn't exist, then create item
         item_id = random.randint(1000, 100000) # this line should delete after DB modified to rowid
-        cursor = db.execute("INSERT INTO ITEM(item_id, name, type, stock) VALUES (?, ?, ?, ?)", [item_id, item_name, item_type, item_quantity])
-        #db.commit()
+        cursor = db.execute("INSERT INTO ITEM(item_id, name, type, stock) VALUES (?, ?, ?, ?)", [item_id, item_name, item_type, item_quantity]) # This line should be modified after DB modified
         print( "This item doesn't exist!")
       else:
         # item exist, get item stock then increase item quantity
@@ -73,19 +83,28 @@ def create_purchase_order():
         print("initial stock: "+str(initial_item_stock))
         new_item_stock = initial_item_stock + item_quantity
         cursor = db.execute("UPDATE ITEM SET stock = ? WHERE name = ? AND type = ?", [new_item_stock, item_name, item_type])
+
         cursor = db.execute("SELECT stock FROM ITEM WHERE name = ? AND type = ?", [item_name, item_type])
         rows = cursor.fetchall()
         final_item_stock = rows[0]['stock']
         print("final stock: "+str(final_item_stock))
 
 
-      cursor = db.execute("SELECT rowid, name, stock FROM ITEM WHERE name = ? AND type = ?", [item_name, item_type])
+      cursor = db.execute("SELECT rowid FROM ITEM WHERE name = ? AND type = ?", [item_name, item_type])
       rows = cursor.fetchall()
-      print("rowid: "+str(rows[0][0]) + " name: "+str(rows[0][1]) + " stock: "+str(rows[0][2]))
+      item_id = rows[0][0]
 
-      # TODO: insert into increase table
+      # insert into increase table
+      cursor = db.execute("INSERT INTO INCREASE(item_id, p_order_id, unit_cost, item_quantity) VALUES(?,?,?,?)", [item_id, p_order_id, item_unit_cost, item_quantity])
+
+      cursor = db.execute("SELECT * FROM INCREASE WHERE item_id = ? AND p_order_id = ?", [item_id, p_order_id])
+      rows = cursor.fetchall()
+      print("create increase record")
+      print("item_id: "+str(rows[0][0])+" p_order_id: "+str(rows[0][1])+" unit cost: "+str(rows[0][2])+" item quantity: "+str(rows[0][3]))
+
+      db.commit()
 
       
-    return "ok" 
+    return jsonify({"message": "Create purchase order success!"})
   else:
-    return 'Content-Type not supported!'
+    return jsonify({"message": "Content-Type not supported!"})
