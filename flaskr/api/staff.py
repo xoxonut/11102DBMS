ffbc04ae37ecbe9b61aefbc3490c7aaae7babc4e
tanlikfeng <likfengtan@gmail.com>
2023-06-02 20:27:13 +0800

    update member,staff,supplier

--- flaskr/api/staff.py
+++ flaskr/api/staff.py
@@ -1,8 +1,106 @@
-from flask import Blueprint,jsonify
+from flask import Blueprint, jsonify, request
+from flaskr.db import get_db
 
-bp = Blueprint('staff', __name__, url_prefix='/staff')
+bp = Blueprint("staff", __name__, url_prefix="/myErp/staff")
 
-@bp.route('/')
-def index():
-    ret = [{'name':'test','code':1}]
-    return jsonify(ret)
+
+@bp.route("/", methods=["GET"])
+def get_staff():
+    db = get_db()
+    cursor = db.execute("SELECT * FROM staff")
+    staff_members = cursor.fetchall()
+    db.close()
+
+    staff_list = []
+    for staff_member in staff_members:
+        staff_dict = {
+            "staff_id": staff_member["staff_id"],
+            "manager_id": staff_member["manager_id"],
+            "name": staff_member["name"],
+            "entry_date": staff_member["entry_date"],
+        }
+        staff_list.append(staff_dict)
+
+    return jsonify({"staff_members": staff_list})
+
+
+@bp.route("/", methods=["POST"])
+def create_staff():
+    content_type = request.headers.get("Content-Type")
+    if content_type == "application/json":
+        staff_data = request.json
+
+        db = get_db()
+        cursor = db.execute(
+            "INSERT INTO staff (manager_id, name, entry_date) VALUES (?, ?, ?)",
+            (
+                staff_data.get("manager_id"),
+                staff_data.get("name"),
+                staff_data.get("entry_date"),
+            ),
+        )
+        staff_id = cursor.lastrowid
+        db.commit()
+        db.close()
+
+        return jsonify(
+            {"message": "Staff member created successfully!", "staff_id": staff_id}
+        )
+
+    else:
+        return jsonify({"message": "Content-Type not supported!"})
+
+
+@bp.route("/<int:staff_id>", methods=["GET"])
+def get_staff_member(staff_id):
+    db = get_db()
+    cursor = db.execute("SELECT * FROM staff WHERE staff_id = ?", (staff_id,))
+    staff_member = cursor.fetchone()
+    db.close()
+
+    if staff_member is None:
+        return jsonify({"message": "Staff member not found!"}), 404
+
+    staff_dict = {
+        "staff_id": staff_member["staff_id"],
+        "manager_id": staff_member["manager_id"],
+        "name": staff_member["name"],
+        "entry_date": staff_member["entry_date"],
+    }
+
+    return jsonify(staff_dict)
+
+
+@bp.route("/<int:staff_id>", methods=["PUT"])
+def update_staff_member(staff_id):
+    content_type = request.headers.get("Content-Type")
+    if content_type == "application/json":
+        staff_data = request.json
+
+        db = get_db()
+        cursor = db.execute(
+            "UPDATE staff SET manager_id = ?, name = ?, entry_date = ? WHERE staff_id = ?",
+            (
+                staff_data.get("manager_id"),
+                staff_data.get("name"),
+                staff_data.get("entry_date"),
+                staff_id,
+            ),
+        )
+        db.commit()
+        db.close()
+
+        return jsonify({"message": "Staff member updated successfully!"})
+
+    else:
+        return jsonify({"message": "Content-Type not supported!"})
+
+
+@bp.route("/<int:staff_id>", methods=["DELETE"])
+def delete_staff_member(staff_id):
+    db = get_db()
+    cursor = db.execute("DELETE FROM staff WHERE staff_id = ?", (staff_id,))
+    db.commit()
+    db.close()
+
+    return jsonify({"message": "Staff member deleted successfully!"})
--- flaskr/api/supplier.py
+++ flaskr/api/supplier.py
@@ -1,7 +1,156 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+from flaskr.db import get_db
+import datetime
+import random
 
-bp = Blueprint('supplier', __name__, url_prefix='/supplier')
+bp = Blueprint("supplier", __name__, url_prefix="/myErp/supplier")
 
-@bp.route('/')
-def index():
-    return 'Supplier index page'
+
+@bp.route("/", methods=["GET"])
+def read_suppliers():
+    db = get_db()
+    cursor = db.execute(
+        """
+        SELECT S.rowid AS supplier_id, S.staff_id, S.name AS supplier_name, S.entry_date, ST.name AS staff_name
+        FROM SUPPLIER S
+        LEFT JOIN STAFF ST ON S.staff_id = ST.rowid
+    """
+    )
+    rows = cursor.fetchall()
+
+    supplier_list = []
+    for row in rows:
+        supplier_id, staff_id, supplier_name, entry_date, staff_name = row
+        supplier = {
+            "supplier_id": supplier_id,
+            "staff_id": staff_id,
+            "supplier_name": supplier_name,
+            "entry_date": entry_date,
+            "staff_name": staff_name,
+        }
+        supplier_list.append(supplier)
+
+    db.close()
+
+    return jsonify(supplier_list)
+
+
+@bp.route("/", methods=["POST"])
+def create_supplier():
+    content_type = request.headers.get("Content-Type")
+    if content_type == "application/json":
+        staff_id = request.json.get("staff_id")
+        name = request.json.get("name")
+        entry_date = request.json.get("entry_date")
+
+        db = get_db()
+
+        # Check if the staff exists
+        cursor = db.execute(
+            "SELECT COUNT(*) AS result FROM STAFF WHERE rowid=?", [staff_id]
+        )
+        rows = cursor.fetchall()
+        if rows[0]["result"] != 1:
+            return jsonify({"message": "This staff doesn't exist!"})
+
+        # Insert into the supplier table
+        supplier_id = random.randint(1000, 100000)
+        cursor = db.execute(
+            "INSERT INTO SUPPLIER (supplier_id, staff_id, name, entry_date) VALUES (?, ?, ?, ?)",
+            [supplier_id, staff_id, name, entry_date],
+        )
+        db.commit()
+
+        return jsonify({"message": "Supplier created successfully"})
+    else:
+        return jsonify({"message": "Content-Type not supported!"})
+
+
+@bp.route("/<int:supplier_id>", methods=["GET"])
+def get_supplier(supplier_id):
+    db = get_db()
+    cursor = db.execute(
+        """
+        SELECT S.rowid AS supplier_id, S.staff_id, S.name AS supplier_name, S.entry_date, ST.name AS staff_name
+        FROM SUPPLIER S
+        LEFT JOIN STAFF ST ON S.staff_id = ST.rowid
+        WHERE S.rowid = ?
+    """,
+        [supplier_id],
+    )
+    supplier = cursor.fetchone()
+
+    if supplier is None:
+        return jsonify({"message": "Supplier not found"})
+
+    supplier_id, staff_id, supplier_name, entry_date, staff_name = supplier
+    supplier_data = {
+        "supplier_id": supplier_id,
+        "staff_id": staff_id,
+        "supplier_name": supplier_name,
+        "entry_date": entry_date,
+        "staff_name": staff_name,
+    }
+
+    return jsonify(supplier_data)
+
+
+@bp.route("/<int:supplier_id>", methods=["PUT"])
+def update_supplier(supplier_id):
+    content_type = request.headers.get("Content-Type")
+    if content_type == "application/json":
+        staff_id = request.json.get("staff_id")
+        name = request.json.get("name")
+        entry_date = request.json.get("entry_date")
+
+        db = get_db()
+
+        # Check if the supplier exists
+        cursor = db.execute(
+            "SELECT COUNT(*) AS result FROM SUPPLIER WHERE rowid=?", [supplier_id]
+        )
+        rows = cursor.fetchall()
+        if rows[0]["result"] != 1:
+            return jsonify({"message": "Supplier not found"})
+
+        # Check if the staff exists
+        cursor = db.execute(
+            "SELECT COUNT(*) AS result FROM STAFF WHERE rowid=?", [staff_id]
+        )
+        rows = cursor.fetchall()
+        if rows[0]["result"] != 1:
+            return jsonify({"message": "This staff doesn't exist!"})
+
+        # Update the supplier
+        db.execute(
+            "UPDATE SUPPLIER SET staff_id = ?, name = ?, entry_date = ? WHERE rowid = ?",
+            [staff_id, name, entry_date, supplier_id],
+        )
+        db.commit()
+
+        return jsonify({"message": "Supplier updated successfully"})
+    else:
+        return jsonify({"message": "Content-Type not supported!"})
+
+
+@bp.route("/<int:supplier_id>", methods=["DELETE"])
+def delete_supplier(supplier_id):
+    content_type = request.headers.get("Content-Type")
+    if content_type == "application/json":
+        db = get_db()
+
+        # Check if the supplier exists
+        cursor = db.execute(
+            "SELECT COUNT(*) AS result FROM SUPPLIER WHERE rowid=?", [supplier_id]
+        )
+        rows = cursor.fetchall()
+        if rows[0]["result"] != 1:
+            return jsonify({"message": "Supplier not found"})
+
+        # Delete the supplier
+        db.execute("DELETE FROM SUPPLIER WHERE rowid=?", [supplier_id])
+        db.commit()
+
+        return jsonify({"message": "Supplier deleted successfully"})
+    else:
+        return jsonify({"message": "Content-Type not supported!"})
