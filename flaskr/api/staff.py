32c0b035d88893cf675bdd1afd98c81211b6aa58
owen-chw <c044066bo@gmail.com>
2023-06-10 10:27:43 +0800

    fix datetime, return, and url error in staff

--- flaskr/api/staff.py
+++ flaskr/api/staff.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, jsonify, request
 from flaskr.db import get_db
+import datetime
 
 bp = Blueprint("staff", __name__, url_prefix="/staff")
 
 
-@bp.route("/get_staff", methods=["GET"])
+@bp.route("/", methods=["GET"])
 def get_staff():
     db = get_db()
     cursor = db.execute("SELECT * FROM STAFF")
@@ -13,18 +14,24 @@ def get_staff():
     staff_list = []
     for staff_member in staff_members:
         manager_id, name, entry_date = staff_member
-        staff_list.append([manager_id, name, entry_date])
+        staff = {
+            "name": name,
+            "manager_id": manager_id,
+            "entry_date": entry_date
+        }
+        staff_list.append(staff)
 
     db.close()
 
-    return jsonify(staff_list)
+    return jsonify({"staff_list": staff_list})
 
 
-@bp.route("/create_staff", methods=["POST"])
+@bp.route("/", methods=["POST"])
 def create_staff():
     content_type = request.headers.get("Content-Type")
     if content_type == "application/json":
         staff_data = request.json
+        entry_date = datetime.datetime.now().strftime("%c")
 
         db = get_db()
         cursor = db.execute(
@@ -32,7 +39,7 @@ def create_staff():
             (
                 staff_data.get("manager_id"),
                 staff_data.get("name"),
-                staff_data.get("entry_date"),
+                entry_date
             ),
         )
         staff_id = cursor.lastrowid
@@ -47,7 +54,7 @@ def create_staff():
         return jsonify({"message": "Content-Type not supported!"})
 
 
-@bp.route("/update_staff_member", methods=["PUT"])
+@bp.route("/", methods=["PUT"])
 def update_staff_member():
     content_type = request.headers.get("Content-Type")
     if content_type == "application/json":
@@ -69,7 +76,7 @@ def update_staff_member():
         return jsonify({"message": "Content-Type not supported!"})
 
 
-@bp.route("/delete_staff_member", methods=["DELETE"])
+@bp.route("/", methods=["DELETE"])
 def delete_staff_member():
     staff_id = request.json.get("staff_id")
     db = get_db()
