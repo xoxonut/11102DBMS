a2d73388470944fcdcf25d0998083727c6d99f64
Frank <frank.ankch17@gmail.com>
2023-06-16 00:11:27 +0800

    fix : manually implement fk operation

--- flaskr/api/staff.py
+++ flaskr/api/staff.py
@@ -33,51 +33,65 @@ def get_staff():
 @bp.route("/", methods=["POST"])
 def create_staff():
     content_type = request.headers.get("Content-Type")
-    if content_type == "application/json":
-        staff_data = request.json
-        entry_date = datetime.datetime.now().strftime("%c")
+    try:
+        if content_type == "application/json":
+            staff_data = request.json
+            entry_date = datetime.datetime.now().strftime("%c")
+            db = get_db()
+            cursor = db.execute('Select staff_id from staff')
+            staff_ids = cursor.fetchall()
+            if staff_data.get('manager_id')!='' and int(staff_data.get('manager_id')) not in [x['staff_id'] for x in staff_ids]:
+                return {"message": "This manager doesn't exist!"}
+            cursor = db.execute(
+                "INSERT INTO staff (manager_id, name, entry_date) VALUES (?, ?, ?)",
+                (
+                    staff_data.get("manager_id"),
+                    staff_data.get("name"),
+                    entry_date
+                ),
+            )
+            staff_id = cursor.lastrowid
+            db.commit()
+            db.close()
 
-        db = get_db()
-        cursor = db.execute(
-            "INSERT INTO staff (manager_id, name, entry_date) VALUES (?, ?, ?)",
-            (
-                staff_data.get("manager_id"),
-                staff_data.get("name"),
-                entry_date
-            ),
-        )
-        staff_id = cursor.lastrowid
-        db.commit()
-        db.close()
+            return jsonify(
+                {"message": "Staff member created successfully!", "staff_id": staff_id}
+            )
 
-        return jsonify(
-            {"message": "Staff member created successfully!", "staff_id": staff_id}
-        )
-
-    else:
-        return jsonify({"message": "Content-Type not supported!"})
+        else:
+            return jsonify({"message": "Content-Type not supported!"})
+    except Exception as e:
+        return jsonify({"message": str(e)})
 
 
 @bp.route("/", methods=["PUT"])
 def update_staff_member():
-    content_type = request.headers.get("Content-Type")
-    if content_type == "application/json":
-        staff_id = request.json.get("staff_id")
-        name = request.json.get("name")
-        manager_id = request.json.get("manager_id")
-        print(staff_id,name,manager_id)
-        db = get_db()
-        db.execute(
-            "UPDATE STAFF SET manager_id = ?, name = ? WHERE rowid = ?",
-            [manager_id, name, staff_id],
-        )
-        db.commit()
-        db.close()
+    try:
+        content_type = request.headers.get("Content-Type")
+        if content_type == "application/json":
+            staff_id = request.json.get("staff_id")
+            name = request.json.get("name")
+            manager_id = request.json.get("manager_id")
+            print(staff_id,name,manager_id)
+            db = get_db()
+            cursor = db.execute('Select staff_id from staff')
+            staff_ids = cursor.fetchall()
+            if manager_id !='' and int(manager_id) not in [x['staff_id'] for x in staff_ids]:
+                return {"message": "This manager doesn't exist!"}
 
-        return jsonify({"message": "Staff member updated successfully!"})
+            db.execute(
+                "UPDATE STAFF SET manager_id = ?, name = ? WHERE rowid = ?",
+                [manager_id, name, staff_id],
+            )
+            db.commit()
+            db.close()
 
-    else:
-        return jsonify({"message": "Content-Type not supported!"})
+            return jsonify({"message": "Staff member updated successfully!"})
+
+        else:
+            return jsonify({"message": "Content-Type not supported!"})
+    except Exception as e:
+        return jsonify({"message": str(e)})
 
 
 @bp.route("/", methods=["DELETE"])
@@ -96,7 +110,7 @@ def delete_staff_member():
     )
     db.commit()
     
-    db.execute("UPDATE STAFF SET manager_id = NULL WHERE manager_id = ?", [staff_id])
+    db.execute("UPDATE STAFF SET manager_id = '' WHERE manager_id = ?", [staff_id])
     db.commit()
     db.close()
     return jsonify({"message": "Staff member deleted successfully!"})
